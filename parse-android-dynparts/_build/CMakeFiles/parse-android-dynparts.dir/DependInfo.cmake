15819ab1f4ed079956b893b2474e30b0fed1fce8
Jordieboyz <boer.jort@gmail.com>
2023-04-25 17:20:32 +0200

    Initial parse-android-dynparts commit

---
+++ parse-android-dynparts/_build/CMakeFiles/parse-android-dynparts.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/sfos/sfbs-install/sfbootstrap/src/hybris-18.1/hybris/parse-android-dynparts/parse-android-dynparts/main.cpp" "/home/sfos/sfbs-install/sfbootstrap/src/hybris-18.1/hybris/parse-android-dynparts/parse-android-dynparts/_build/CMakeFiles/parse-android-dynparts.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../liblp/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/sfos/sfbs-install/sfbootstrap/src/hybris-18.1/hybris/parse-android-dynparts/parse-android-dynparts/_build/liblp/CMakeFiles/lp.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
